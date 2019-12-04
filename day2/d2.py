149b5d1c0348cf5b04c367df313a6ece26736b1a
Paul Medeiros <paulmedeiros92@gmail.com>
2019-12-03 20:43:46 -0500

    wip

--- day2/d2.py
+++ day2/d2.py
@@ -1,7 +1,15 @@
 f = open("day2/input.txt", "r")
 values = f.read().split(",")  # no empty space at the end of input file
 
-for i in range(len(values)):
-  
+for i in range(len(values)/4):
+  chunk = values[i: i+3]
+  if 99 in chunk:
+    break
 
-print(values)
+  if values[op] == 1:
+    values[target] = values[x] + values[y]
+  
+  if values[op] == 2:
+    values[target] = values[x] * values[y]
+
+print(values)  
